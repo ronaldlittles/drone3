70db32ebbe0b48cab24291ef6684c21738855cd9
ronaldlittles <63035705+ronaldlittles@users.noreply.github.com>
2023-11-18 10:47:35 -0800

    got

--- sources/Experience/fragment.js
+++ sources/Experience/fragment.js
@@ -20,33 +20,10 @@ const fragmentShader = {
         }
       }
 
-`
+`,
     }
 
-   const fragmentShader2 = { 
-  fragmentShader2: `
-
-
-varying vec2 vUv;
-
-
-
-void main() {
-  float squareSize = 5.0; 
-
-  vec2 position = floor(vUv / squareSize);
-
-  if (mod(position.x + position.y, 2.0) < 1.0) {
-
-    gl_FragColor = vec4(0.6, 1.0, 0.9, 1.0); 
-  } else {
-    gl_FragColor = vec4(0.0,0.0, 0.0, 1.0); 
-  }
-
-}
-
-`
-   }
+  
 
 
 
