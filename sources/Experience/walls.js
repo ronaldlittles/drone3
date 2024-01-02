f545ca84a05a23ae0636d3d1798135cb37a78242
ronaldlittles <63035705+ronaldlittles@users.noreply.github.com>
2024-01-01 20:21:09 -0800

    90px;

--- sources/Experience/walls.js
+++ sources/Experience/walls.js
@@ -268,7 +268,7 @@ export default class Walls extends EventEmitter {
           this.label.style.color = "white";
           //this.label.style.padding = "5px";
           this.label.style.fontFamily = "sans-serif";
-          this.label.style.fontSize = "56px";
+          this.label.style.fontSize = "36px";
           this.label.style.textShadow = "1px 2px #000000";
           this.label.style.pointerEvents = "none"; // Make sure the this.label doesn't interfere with mouse events
           document.body.appendChild(this.label);
